a22dc813837b9e4b26409cfa0360c8e165649aa8
Sprinkker <wainwang98@gmail.com>
2024-11-04 03:11:19 +1000

    DECO7140 ShunQi Wang 47976829 Project
    DECO7140 ShunQi Wang 47976829 Project

---
+++ DECO7140 ShunQi Wang 47976829/js/comments.js
@@ -0,0 +1,82 @@
+let comments_list = [
+  {
+    "id": 1,
+    "name": "Alice",
+    "avatar": "./assets/avatar1.jpg",
+    "content": "Sydney Opera House is a masterpiece of architecture!",
+    "time": "2024-11-01 12:00:00",
+    "comment_up_num": 34
+  },
+  {
+    "id": 2,
+    "name": "Bob",
+    "avatar": "./assets/avatar2.jpg",
+    "content": "Nothing compares to the beauty of Sydney Harbour at sunset!",
+    "time": "2024-11-01 12:01:00",
+    "comment_up_num": 45
+  },
+  {
+    "id": 3,
+    "name": "Charlie",
+    "avatar": "./assets/avatar3.jpg",
+    "content": "Kangaroo Island is pure magic, especially for nature lovers!",
+    "time": "2024-11-01 12:02:00",
+    "comment_up_num": 50
+  },
+  {
+    "id": 4,
+    "name": "Diana",
+    "avatar": "./assets/avatar4.jpg",
+    "content": "The drive along Great Ocean Road is unforgettable!",
+    "time": "2024-11-01 12:03:00",
+    "comment_up_num": 65
+  },
+  {
+    "id": 5,
+    "name": "Eli",
+    "avatar": "./assets/avatar5.jpg",
+    "content": "Cradle Mountain is an adventure lover’s paradise!",
+    "time": "2024-11-01 12:04:00",
+    "comment_up_num": 32
+  },
+  {
+    "id": 6,
+    "name": "Fiona",
+    "avatar": "./assets/avatar6.jpg",
+    "content": "The Opera House is an icon of Australia for a reason!",
+    "time": "2024-11-01 12:05:00",
+    "comment_up_num": 70
+  },
+  {
+    "id": 7,
+    "name": "George",
+    "avatar": "./assets/avatar7.jpg",
+    "content": "Sydney Harbour cruises are an absolute must!",
+    "time": "2024-11-01 12:06:00",
+    "comment_up_num": 55
+  },
+  {
+    "id": 8,
+    "name": "Hannah",
+    "avatar": "./assets/avatar8.jpg",
+    "content": "Kangaroo Island has some of the best wildlife experiences!",
+    "time": "2024-11-01 12:07:00",
+    "comment_up_num": 40
+  },
+  {
+    "id": 9,
+    "name": "Ian",
+    "avatar": "./assets/avatar9.jpg",
+    "content": "The Twelve Apostles on Great Ocean Road are breathtaking!",
+    "time": "2024-11-01 12:08:00",
+    "comment_up_num": 53
+  },
+  {
+    "id": 10,
+    "name": "Jane",
+    "avatar": "./assets/avatar10.jpg",
+    "content": "Cradle Mountain has the most stunning trails!",
+    "time": "2024-11-01 12:09:00",
+    "comment_up_num": 48
+  }
+];
